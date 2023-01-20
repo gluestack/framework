136df51d8634aa3fcb592629d94c24234c1dbd66
Faiz A. Farooqui <faizz.af@gmail.com>
2023-01-20 16:54:54 +0530

    chore(v0.0.21): updated the list of plugins

--- constants/plugins.js
+++ constants/plugins.js
@@ -1,44 +1,60 @@
 const PLUGINS = {
+	framework: {
+		package: '@gluestack/glue-plugin-framework',
+		description: 'Installs a GlueStack Framework Plugin',
+	},
 	web: {
-		package: '@gluestack/web',
+		package: '@gluestack/glue-plugin-web',
 		description: 'Installs a Next.js App Plugin',
+	},
+	storybook: {
+		package: '@gluestack/glue-plugin-storybook',
+		description: 'Installs a Storybook App Plugin',
 	},
 	mobile: {
 		package: '@gluestack/mobile',
 		description: 'Installs a React Native App Plugin',
 	},
-	storybook: {
-		package: '@gluestack/storybook',
-		description: 'Installs a Storybook App Plugin',
+	engine: {
+		package: '@gluestack/glue-plugin-engine',
+		description: 'Installs a GlueStack Process Manager Engine Plugin',
 	},
-	functions: {
-		package: '@gluestack/glue-plugin-functions',
-		description: 'Installs a Function for computing',
+	postgres: {
+		package: '@gluestack/glue-plugin-postgres',
+		description: 'Installs a Postgres Database Plugin',
+	},
+	'pg-admin': {
+		package: '@gluestack/glue-plugin-pg-admin',
+		description: 'Installs a PG Admin Database Plugin',
 	},
 	graphql: {
 		package: '@gluestack/glue-plugin-graphql',
 		description: 'Installs a Hasura GraphQL App Plugin',
 	},
-	engine: {
-		package: '@gluestack/glue-plugin-engine',
-		description: 'Installs a Backend GlueStack Engine App Plugin',
+	functions: {
+		package: '@gluestack/glue-plugin-functions',
+		description: 'Installs a Function for computing',
+	},
+	'functions.action': {
+		package: '@gluestack/glue-plugin-functions.action',
+		description: 'Installs Hasura GraphQL Action Management on top of Functions Plugin',
+	},
+	'backend-engine': {
+		package: '@gluestack/glue-plugin-backend-engine',
+		description: 'Installs a Backend GlueStack Engine App Plugin with CRONs & Queues'
 	},
 	auth: {
-		package: '@gluestack/auth',
+		package: '@gluestack/glue-plugin-auth',
 		description: 'Installs auth in your App',
 	},
+	minio: {
+		package: '@gluestack/glue-plugin-minio',
+		description: 'Installs a Minio Storage Plugin',
+	},
 	storage: {
-		package: '@gluestack/storage',
+		package: '@gluestack/glue-plugin-storage',
 		description: 'Installs storage in your App',
-	},
-	cron: {
-		package: '@gluestack/cron',
-		description: 'Installs Cron in your App',
-	},
-	jobs: {
-		package: '@gluestack/jobs',
-		description: 'Installs Jobs in your App',
-	},
+	}
 };
 
 module.exports = { PLUGINS };
