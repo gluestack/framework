71250223c5e510278fc56faf90fd796037e4bef3
madhav23bansal <madhavb230100@gmail.com>
2023-02-15 12:57:40 +0530

    fix: added overview heading

--- storybook/.storybook/preview.js
+++ storybook/.storybook/preview.js
@@ -1,40 +1,43 @@
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  docs: {
-    // inlineStories: false,
-  },
-  options: {
-    storySort: {
-      method: "",
-      order: [
-        "Introduction",
-        "Architecture",
-        "Philosophy",
-        "Prerequisites",
-        "Installation",
-        "Why gluestack?",
-        "Goals and non-goals",
-        "FAQs",
-        "Usage",
-        [
-          "Your first project",
-          [
-            "Bootstrap a project",
-            "The glue CLI",
-            "Install the web plugin",
-            "Adding the database and GraphQL",
-            "Consuming GraphQL",
-          ],
-          "Testing gluestack",
-        ],
-      ],
-      locales: "",
-    },
-  },
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+	docs: {
+		// inlineStories: false,
+	},
+	options: {
+		storySort: {
+			method: '',
+			order: [
+				'Overview',
+				[
+					'Introduction',
+					'Architecture',
+					'Philosophy',
+					'Prerequisites',
+					'Installation',
+					'Why gluestack?',
+					'Goals and non-goals',
+					'FAQs',
+				],
+				'Usage',
+				[
+					'Your first project',
+					[
+						'Bootstrap a project',
+						'The glue CLI',
+						'Install the web plugin',
+						'Adding the database and GraphQL',
+						'Consuming GraphQL',
+					],
+					'Testing gluestack',
+				],
+			],
+			locales: '',
+		},
+	},
 };
